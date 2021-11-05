c930c47e6cfe58774bcb650403b6b2aa7c483b12
Walter Walden <Waldman200036@gmail.com>
2021-11-05 14:57:16 -0500

    new file:   public/css/bootstrap.min.css 	new file:   public/css/images/dha_medicine.jpg 	new file:   public/css/styles.css 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/UI/Body.js 	new file:   src/components/UI/Body.module.css 	new file:   src/components/UI/Footer.js 	new file:   src/components/UI/Header.js

---
+++ src/components/UI/Body.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+
+const Body = ()=>{
+return (
+  <Fragment>
+    <div class="container">
+      <div class="row"></div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="panel">
+            <div class="panel-body">
+              <h2>This site contains:</h2>
+              <ul>
+                <li>
+                  <a href="/questionnaire">DQMC Training Survey </a>
+                </li>
+                <li>
+                  <a href="/dqmcTrainingRegistration?form=newDQManager">
+                    DQMC Training Registration{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Fragment>
+);
+}
+
+export default Body;
