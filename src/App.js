07fe6729db92cafad347b073dfb15ea840c59e78
Walter Walden <Waldman200036@gmail.com>
2021-11-15 07:58:07 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Body from './components/UI/Body';
+import Footer from './components/UI/Footer';
+import Header from './components/UI/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
